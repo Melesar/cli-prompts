031661e64f904b61d411f4e5befcd373f787ec08
Melesar <sergey.pikhota@protonmail.com>
2023-05-30 13:22:05 +0200

    Add documentation for the Options struct

--- src/prompts/options/mod.rs
+++ src/prompts/options/mod.rs
@@ -2,6 +2,7 @@ pub mod multiselect;
 pub mod selection;
 pub mod multioption_prompt;
 
+/// A helper struct for the multi-option prompts
 pub struct Options<T> {
     all_options: Vec<T>,
     transformed_options: Vec<String>,
@@ -12,6 +13,7 @@ impl<T> Options<T>
 where
     T: Into<String> + Clone,
 {
+    /// Create `Options` from an iterator over a type that is convertable to `String`
     pub fn from_iter<I>(iter: I) -> Self
     where
         I: Iterator<Item = T>,
@@ -27,6 +29,7 @@ where
 }
 
 impl<T> Options<T> {
+    /// Create `Options` from an arbitrary type using provided transformation function to `String`
     pub fn from_iter_transformed<I, F>(iter: I, transformation: F) -> Self
     where
         I: Iterator<Item = T>,
@@ -43,6 +46,7 @@ impl<T> Options<T> {
         }
     }
 
+    /// Filter options using provided string slice
     pub fn filter(&mut self, filter: &str) {
         self.filtered_options.clear();
         for (index, option) in self.transformed_options.iter().enumerate() {
@@ -52,14 +56,17 @@ impl<T> Options<T> {
         }
     }
 
+    /// Retrieve the indices of all options that satisfy the last applied filter
     pub fn filtered_options(&self) -> &[usize] {
         &self.filtered_options
     }
 
+    /// Get a mutable reference to the vector all available options
     pub fn all_options_mut(&mut self) -> &mut Vec<T> {
         &mut self.all_options
     }
 
+    /// Get a reference to all options in their string representation
     pub fn transformed_options(&self) -> &[String] {
         &self.transformed_options
     }
